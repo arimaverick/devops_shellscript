8054a978c6c4e6b727d44cd98bee1bff7b8092f6
arimaverick <31853801+arimaverick@users.noreply.github.com>
2017-09-17 01:00:04 +0530

    Update gitshell.sh

--- gitshell.sh
+++ gitshell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 git clone "$1"
-name=$(echo `basename "git@github.com:arimaverick/devops_shellscript.git"` | cut -f 1 -d '.')
+name=$(echo `basename "$1"` | cut -f 1 -d '.')
 cd $name
 mv index.html index_jenkins.html
 git add .
